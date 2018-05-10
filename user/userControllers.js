4c9404a833d5d38f579931e545ed2f30a48f0b09
Ridima14 <ahuja.ridima14@gmail.com>
2018-05-11 00:10:01 +0530

    Final

--- user/userControllers.js
+++ user/userControllers.js
@@ -4,10 +4,7 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 var User = require('./User');
 
-
-
 // CREATES A NEW USER
-
 router.post('/myaction', function (req, res) {
     User.create({
             name : req.body.name,
@@ -20,16 +17,18 @@ router.post('/myaction', function (req, res) {
         });
 });
 
+router.get('/',function(req,res){
+  res.render('register');
+})
 
 // RETURNS ALL THE USERS IN THE DATABASE
-router.get('/', function (req, res) {
+router.get('/API/users', function (req, res) {
     User.find({}, function (err, users) {
         if (err) return res.status(500).send("There was a problem finding the users.");
         res.status(200).send(users);
     });
 });
 module.exports = router;
-
 
 // GETS A SINGLE USER FROM THE DATABASE
 router.get('/:id', function (req, res) {
@@ -40,7 +39,6 @@ router.get('/:id', function (req, res) {
     });
 });
 
-
 // DELETES A USER FROM THE DATABASE
 router.delete('/:id', function (req, res) {
     User.findByIdAndRemove(req.params.id, function (err, user) {
@@ -48,10 +46,8 @@ router.delete('/:id', function (req, res) {
         res.status(200).send("User "+ user.name +" was deleted.");
     });
 });
-
 // UPDATES A SINGLE USER IN THE DATABASE
 router.put('/:id', function (req, res) {
-
     User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
         if (err) return res.status(500).send("There was a problem updating the user.");
         res.status(200).send(user);
