2983bbb413f18d351b3968fdfc7ced46e3dfdfbf
Ridima14 <ahuja.ridima14@gmail.com>
2018-05-11 17:06:25 +0530

    final

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ app.set('view engine', 'ejs');
 var db = require('./db');
 
 
-var UserController = require('./user/UserControllers');
+var UserController = require('./user/userControllers');
 app.use('/users', UserController);
 
 var routes = require('./user/userControllers');
 app.use('/', routes);
 
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
 
 module.exports = app;
